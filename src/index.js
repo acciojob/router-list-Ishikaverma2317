a9fe6400dc5db71cc538d4a160c2ac25e340c897
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-31 20:39:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
