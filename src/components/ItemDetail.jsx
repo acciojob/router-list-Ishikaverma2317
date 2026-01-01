dface0f3bfd135521c01ca0112cd0d84d4eb0b9d
Ishika Verma <ishikaverma2317@gmail.com>
2026-01-01 10:42:05 +0530

    Update ItemDetail.jsx

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-const itemData = {
-  1: { name: "Item 1", description: "Description for Item 1" },
-  2: { name: "Item 2", description: "Description for Item 2" },
-  3: { name: "Item 3", description: "Description for Item 3" },
-};
-
 function ItemDetail() {
   const { id } = useParams();
-  const item = itemData[id];
 
   return (
     <div>
-      <h1>{item.name}</h1>
-      <p>{item.description}</p>
+      <h1>Item {id}</h1>
+      <p>Description for Item {id}</p>
     </div>
   );
 }
